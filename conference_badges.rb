73659c4596ce88980d12e38aa8787c01a9296bc6
theo <teddyhoover@gmail.com>
2020-12-09 14:46:42 -0600

    commit tree 3799f8a10c8085408dc51e7fb82d63a561d39a55

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    badges = []
+    names.each do |name| #bad convention too simillar names
+        badges.push("Hello, my name is #{name}.")
+    end
+    badges
+end
+
+def assign_rooms(guests)
+    welcomes = []
+    guests.each_with_index do |name, room|
+        welcomes.push("Hello, #{name}! You'll be assigned to room #{room + 1}!")
+    end
+    welcomes
+end
+
+def printer(attendees)
+    #puts #"Hello, Edsger! You'll be assigned to room 1!"
+    assign_rooms(attendees).each do |out|
+        puts out
+    end
+    batch_badge_creator(attendees).each do |out|
+        puts out
+    end
+end
